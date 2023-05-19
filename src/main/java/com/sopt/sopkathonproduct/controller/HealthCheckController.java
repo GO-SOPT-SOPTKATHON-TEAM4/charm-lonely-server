9a22c12ae429a2550c5822edb1fad281c775a293
unsualc@likelion.org <unusualc@likelion.org>
2023-05-20 01:26:57 +0900

    initial setting

--- src/main/java/com/sopt/sopkathonproduct/controller/HealthCheckController.java
+++ src/main/java/com/sopt/sopkathonproduct/controller/HealthCheckController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping
+    @GetMapping ("/api/health")
     public ResponseEntity<ApiResponseDto> healthCheck() {
         return ResponseEntity.ok(ApiResponseDto.success(SuccessStatus.HEALTH));
     }
