8a80febdb4cae736036d958f34f379f1efd537b8
이빵주 <yungju9982@naver.com>
2023-05-20 23:23:19 +0900

    #1 [fix] 에러메세지 수정

--- src/main/java/com/sopt/sopkathonproduct/exception/ErrorStatus.java
+++ src/main/java/com/sopt/sopkathonproduct/exception/ErrorStatus.java
@@ -17,7 +17,7 @@ public enum ErrorStatus {
     /*
     NOT_FOUND
      */
-    NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 리소스입니다."),
+    NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 게시물이 존재하지 않습니다."),
 
     /*
     BAD_REQUEST
@@ -31,7 +31,7 @@ public enum ErrorStatus {
     /*
     SERVER_ERROR
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생하였습니다."),
     BAD_GATEWAY_EXCEPTION(HttpStatus.BAD_GATEWAY, "일시적인 에러가 발생하였습니다.\n잠시 후 다시 시도해주세요!"),
     SERVICE_UNAVAILABLE_EXCEPTION(HttpStatus.SERVICE_UNAVAILABLE, "현재 점검 중입니다.\n잠시 후 다시 시도해주세요!"),
     ;
