f872101ec6c68759963d66639f145c7d0d4a2278
이빵주 <yungju9982@naver.com>
2023-05-21 00:48:06 +0900

    #5 [feat] PostService 구현

--- src/main/java/com/sopt/sopkathonproduct/service/PostService.java
+++ src/main/java/com/sopt/sopkathonproduct/service/PostService.java
@@ -2,6 +2,8 @@ package com.sopt.sopkathonproduct.service;
 
 
 import com.sopt.sopkathonproduct.domain.entity.Post;
+import com.sopt.sopkathonproduct.exception.ErrorStatus;
+import com.sopt.sopkathonproduct.exception.NotFoundException;
 import com.sopt.sopkathonproduct.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,4 +15,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
 
     private final PostRepository postRepository;
+
+    // TODO 2zerozu 추후 구현 예정
+/*    public List<Post> getAll() {
+
+    }*/
+
+    public Post getById(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_EXCEPTION));
+
+        return Post.newInstance(
+                post.getId(),
+                post.getNickname(),
+                post.getImageUrl(),
+                post.getComment()
+        );
+    }
 }
